498e3d4b823a2949fe861f02fcb73d3c31cc25c4
Karlosia <karolina.burdzik@onet.pl>
2024-12-08 20:49:26 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,24 @@
+﻿namespace Simulator;
+    public class Creature
+    {
+    public string Name { get; init; } = "Noname";
+    private int level = 1;
+    public int Level
+    {
+        get => level;
+        set => level = value > 0 ? value : 0;
+    }
+
+    public Creature(string name, int level=1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() { }
+
+    public string Info => $"Name:{Name} [{Level}]";
+
+    public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}");
+
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -4,6 +4,8 @@ internal class Program
         public static void Main(string[] args)
         {
             Console.WriteLine("Starting Simulator!\n");
+
         }
+
     }
 
