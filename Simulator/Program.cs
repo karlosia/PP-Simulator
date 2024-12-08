7461657d89b1fcc95d95f13a680caa3ef05529db
Karlosia <karolina.burdzik@onet.pl>
2024-12-08 20:28:16 +0100

    Add project files.

---
+++ Simulator/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Simulator;
+internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Simulator!\n");
+        }
+    }
+
