7257e14893727f34a48195c5d7ba1d0ea7aadf2f
Karlosia <karolina.burdzik@onet.pl>
2024-12-13 12:43:44 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -2,7 +2,34 @@
 
 public class Animals
 {
-    public required string Description { get; init; }
+    private string description = "Unknown";
+
+    public string Description
+    {
+        get => description;
+        init
+        {
+            var trimmed = value?.Trim();
+
+            if (string.IsNullOrWhiteSpace(trimmed))
+            {
+                description = "Unknown";
+            }
+            else
+            {
+                if (trimmed.Length>15)
+                {
+                    trimmed = trimmed.Substring(0, 15);
+                }
+                trimmed = trimmed.TrimEnd();
+                if (trimmed.Length<3)
+                {
+                    trimmed = trimmed.PadRight(3, '#');
+                }
+                description = trimmed;
+            }
+        }
+    }
     public uint Size { get; set; } = 3;
     public string Info => $"{Description} <{Size}>";
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,12 +1,46 @@
 ﻿namespace Simulator;
     public class Creature
     {
-    public string Name { get; init; } = "Noname";
+    public string name = "Unknown";
     private int level = 1;
+
+
+    public string Name
+    {
+        get => name;
+        init
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                name = "Unknown";
+            }
+            else
+            {
+                string trimmed = value.Trim();
+                if (trimmed.Length < 3)
+                {
+                    trimmed = trimmed.PadRight(3, '#');
+                }
+                else if (trimmed.Length > 25)
+                {
+                    trimmed = trimmed.Substring(0, 25).TrimEnd();
+                    if (trimmed.Length < 3)
+                    {
+                        trimmed = trimmed.PadRight(3, '#');
+                    }
+                }
+                if (char.IsLower(trimmed[0]))
+                {
+                    trimmed = char.ToUpper(trimmed[0]) + trimmed.Substring(1);
+                }
+                name = trimmed;
+            }
+        }
+    }
     public int Level
     {
         get => level;
-        set => level = value > 0 ? value : 0;
+        init { level = Math.Clamp(value, 1, 10); }
     }
 
     public Creature(string name, int level=1)
@@ -21,4 +55,8 @@
 
     public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}");
 
+    public void Upgrade()
+    {
+        level = Math.Min(level + 1, 10);
+    }
 }
