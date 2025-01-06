8f4d1445098d1a26e03d92df509cd5346a6bda7c
Karlosia <karolina.burdzik@onet.pl>
2025-01-06 13:59:36 +0100

    Mapy

---
+++ Simulator/Maps/SmallSquareMap.cs
@@ -0,0 +1,41 @@
+﻿namespace Simulator.Maps;
+
+internal class SmallSquareMap : Map
+{
+    public int Size { get; }
+
+    private readonly Rectangle mapArea;
+    
+
+    public SmallSquareMap(int size)
+    {
+        if (size < 5 || size > 20)
+        {
+            throw new ArgumentOutOfRangeException("Niepoprawny rozmiar mapy. Mapa powinna mieć rozmiar od 5 do 20 punktów.");
+        }
+
+        Size = size;
+        mapArea = new Rectangle(0, 0, Size - 1, Size - 1);
+    }
+    
+    public override bool Exist(Point p)
+    {
+        return mapArea.Contains(p);
+    }
+
+    public override Point Next(Point p, Direction d)
+    {
+        Point nextPoint = p.Next(d);
+        if (Exist(nextPoint))
+            return nextPoint;
+        return p;
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        Point nextPoint = p.NextDiagonal(d);
+        if (Exist(nextPoint))
+            return nextPoint;
+        return p;
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,38 +1,45 @@
 ﻿namespace Simulator;
+
+using Simulator.Maps;
 internal class Program
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
-        Lab5a();
+        Lab5b();
 
     }
-    public static void Lab5a()
+    public static void Lab5b()
     {
-
-        Console.WriteLine("== Tworzenie prostokątów ==");
-
-        Rectangle firstrec = new Rectangle(1, 4, 3, 8);
-        Console.WriteLine("Pierwszy prostokąt: " + firstrec);
-
-        Rectangle secrec = new Rectangle(9, 6, 1, 3);
-        Console.WriteLine("Drugi prostokąt: " + secrec);
-
-
-        Point p1 = new Point(2, 2);
-        Point p2 = new Point(3, 3);
-        Rectangle thirdrec = new Rectangle(p1, p2);
-        Console.WriteLine("Trzeci prostokąt: " + thirdrec);
-
         try
         {
-            Rectangle rect4 = new Rectangle(3, 3, 3, 6);
+            Map firstmap = new SmallSquareMap(4);
         }
-        catch (ArgumentException ex)
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+
+        }
+        try
+        {
+            Map secondmap = new SmallSquareMap(25);
+        }
+        catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
         }
 
+        Map map = new SmallSquareMap(10);
+        Console.WriteLine("Mapa o rozmiarze 10 została utworzona.");
+        
+        Point point1 = new Point(7, 3);
+        Point point2 = new Point(12, 12);
+
+        Console.WriteLine($"Czy punkt {point1} istnieje w mapie? {map.Exist(point1)}");
+        Console.WriteLine($"Czy punkt {point2} istnieje w mapie? {map.Exist(point2)}");
+
+        Console.WriteLine($"Następny punkt po {point1} w lewo: {map.Next(point1, Direction.Left)}");
+        Console.WriteLine($"Następny punkt po {point1} w lewo (przekątna): {map.NextDiagonal(point1, Direction.Left)}");
     }
 }
 
