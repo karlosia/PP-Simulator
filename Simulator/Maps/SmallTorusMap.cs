ae7db3aeac57ae7386f5bf9ce35a0f3fbb818d80
Karlosia <karolina.burdzik@onet.pl>
2025-01-07 17:03:50 +0100

    TDD

---
+++ Simulator/Maps/SmallTorusMap.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace Simulator.Maps;
+
+public class SmallTorusMap : Map
+{
+
+    public int Size { get;  }
+    
+
+    public SmallTorusMap(int size)
+    {
+        if (size < 5 || size > 20)
+        {
+            throw new ArgumentOutOfRangeException("Rozmiar musi być od 5 do 10");
+        }
+        Size = size;
+        
+    }
+
+    public override bool Exist(Point p)
+    {
+        return p.X >= 0 && p.X < Size && p.Y >= 0 && p.Y < Size;
+    }
+
+    public override Point Next(Point p, Direction d)
+    {
+        var nextPoint = p.Next(d);
+
+        int wrappedX = (nextPoint.X + Size) % Size;
+        int wrappedY = (nextPoint.Y + Size) % Size;
+
+        return new Point(wrappedX, wrappedY);
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        var nextPoint = p.NextDiagonal(d);
+
+        int wrappedX = (nextPoint.X + Size) % Size;
+        int wrappedY = (nextPoint.Y + Size) % Size;
+
+        return new Point(wrappedX, wrappedY);
+    }
+}
